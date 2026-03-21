38958ece590cd21c4e094658b05981bb6ad948ee
Anasstasiya Sviridenkova <anastasia_sviridenkova@mail.ru>
2026-03-21 23:12:31 +0500

    downgrade Spring Boot to 3.4.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ repositories {
 }
 
 plugins {
-	id("org.springframework.boot") version "3.5.9"
+	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
 
 	application
